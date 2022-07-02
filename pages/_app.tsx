11e554a8efd9e772206c36e1f237f96d7b8ae03a
rishabh amga <rishabhamga@rishabhs-MacBook-Pro.local>
2022-07-02 22:52:28 +0530

    ready to deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { MoralisProvider } from 'react-moralis'
+import { GunProvider } from '../context/gunContext'
+import { CoinMarketProvider } from '../context/context'
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <MoralisProvider
+      serverUrl='https://crgblcpkrta7.usemoralis.com:2053/server'
+      appId='3NBpsg10JuFpT6hKXyBZYwxBoaHQLWjFvU8UFitr'
+    >
+      <GunProvider>
+        <CoinMarketProvider>
+          <Component {...pageProps} />
+        </CoinMarketProvider>
+      </GunProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
+
